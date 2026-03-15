c8edc5d37fb44f801f983e880af050291db2b04c
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-03-15 21:26:08 +0100

    Make sure to dispose CancellationTokenSource in RunningGamesManager

--- src/RayCarrot.RCP.Metro/Games/RunningGamesManager.cs
+++ src/RayCarrot.RCP.Metro/Games/RunningGamesManager.cs
@@ -150,6 +150,7 @@ public class RunningGamesManager
             return;
 
         CancellationTokenSource.Cancel();
+        CancellationTokenSource.Dispose();
         CancellationTokenSource = null;
     }
 
