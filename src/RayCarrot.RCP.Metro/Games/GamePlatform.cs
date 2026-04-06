f210b22075b711ea33b521aeade493e727bcda30
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2026-04-06 13:49:09 +0200

    Update order for game platforms

--- src/RayCarrot.RCP.Metro/Games/GamePlatform.cs
+++ src/RayCarrot.RCP.Metro/Games/GamePlatform.cs
@@ -5,6 +5,8 @@
 /// </summary>
 public enum GamePlatform
 {
+    // NOTE: Order is PC first, then by release
+
     /// <summary>
     /// MS-DOS
     /// </summary>
@@ -26,36 +28,36 @@ public enum GamePlatform
     /// <summary>
     /// Atari Jaguar
     /// </summary>
-    [GamePlatformInfo(nameof(Resources.Platform_Jaguar), GamePlatformIconAsset.Jaguar)]
+    [GamePlatformInfo(nameof(Resources.Platform_Jaguar), GamePlatformIconAsset.Jaguar)] // 1993
     Jaguar,
 
     /// <summary>
     /// PlayStation
     /// </summary>
-    [GamePlatformInfo(nameof(Resources.Platform_Ps1), GamePlatformIconAsset.Ps1)]
+    [GamePlatformInfo(nameof(Resources.Platform_Ps1), GamePlatformIconAsset.Ps1)] // 1994
     Ps1,
-
-    /// <summary>
-    /// PlayStation
-    /// </summary>
-    [GamePlatformInfo(nameof(Resources.Platform_Ps2), GamePlatformIconAsset.Ps2)]
-    Ps2,
 
     /// <summary>
     /// Game Boy Color
     /// </summary>
-    [GamePlatformInfo(nameof(Resources.Platform_Gbc), GamePlatformIconAsset.Gbc)]
+    [GamePlatformInfo(nameof(Resources.Platform_Gbc), GamePlatformIconAsset.Gbc)] // 1998
     Gbc,
+
+    /// <summary>
+    /// PlayStation
+    /// </summary>
+    [GamePlatformInfo(nameof(Resources.Platform_Ps2), GamePlatformIconAsset.Ps2)] // 2000
+    Ps2,
 
     /// <summary>
     /// Game Boy Advance
     /// </summary>
-    [GamePlatformInfo(nameof(Resources.Platform_Gba), GamePlatformIconAsset.Gba)]
+    [GamePlatformInfo(nameof(Resources.Platform_Gba), GamePlatformIconAsset.Gba)] // 2001
     Gba,
 
     /// <summary>
     /// GameCube
     /// </summary>
-    [GamePlatformInfo(nameof(Resources.Platform_GameCube), GamePlatformIconAsset.GameCube)]
+    [GamePlatformInfo(nameof(Resources.Platform_GameCube), GamePlatformIconAsset.GameCube)] // 2001
     GameCube,
 }
